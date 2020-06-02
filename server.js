fc7462509507bc95b4e7e43e791d3c875f385cb2
Rahman Mustafayev <mustafayev7rahman@gmial.com>
2020-06-02 09:59:01 +0400

    reading with fin bit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const net = require('net');
-const crypto = require('crypto')
+const crypto = require('crypto');
+const sql = require('mssql')
 const port = 12345;
-const regEx = /Sec-WebSocket-Key: (.*)/m
+const regEx = /Sec-WebSocket-Key: (.*)/m;
+
 const server = net.createServer((client) => {
   client.on('data', (data) => {
     const bufferString = data.toString('utf8');
@@ -15,7 +17,7 @@ const server = net.createServer((client) => {
       client.write('Connection: Upgrade\r\n');
       client.write('Sec-WebSocket-Version: 13\r\n');
       client.write(`Sec-WebSocket-Accept: ${keyB64}\r\n\r\n`);
-      
+
       const id = setInterval(() => {
 
         const buf2 = Buffer.from(new Date().toLocaleTimeString());
@@ -31,12 +33,46 @@ const server = net.createServer((client) => {
 
         const buf1 = buf3.length === 0 ? Buffer.from([129, messageLengthCode]) : Buffer.concat([Buffer.from([129, messageLengthCode]), buf3])
         client.write(Buffer.concat([buf1, buf2]))
-      }, 1000);
+      }, 10000);
       client.on('error', _ => clearInterval(id))
-     
     }
     else {
-      console.log(data);
+      if (data[0] >> 7 == 1) {
+        const opcode = data.slice(0,1) & 0b00001111;
+        const maskBit = data.slice(1,2) & 0b10000000;
+        // console.log(data)
+        if (maskBit == 128) {
+          const messageLengthCode = data.slice(1) & 0b01111111;
+          let messageLength = 0;
+          let maskKeyOffset = 0
+          messageLength = messageLengthCode;
+          maskKeyOffset = 2
+          if (messageLengthCode == 126) {
+            messageLength = data[2, 3];
+            maskKeyOffset = 4
+          }
+          else if (messageLengthCode == 127) {
+            messageLength = data[2, 3, 4, 5];
+            maskKeyOffset = 6
+          }
+          const maskKey = data.slice(maskKeyOffset, maskKeyOffset + 4);
+          // console.log(maskKey);
+          let payloadDecoded = [];
+          const payloadOffset = maskKeyOffset + 4;
+          data.map((byte, index) => {
+            if (index >= payloadOffset) {
+              const decodedByte = byte ^ maskKey [(index - payloadOffset) % 4];
+              payloadDecoded = [...payloadDecoded, decodedByte]
+            }
+          });
+          const bufPayload = Buffer.from(payloadDecoded)
+          // console.log(bufPayload.toString())
+        }
+        else
+          console.log(`maskbit not 1 \n`,  data.slice(1, 2))
+      }
+      else
+        console.log(`not final bit \n`, data.slice(1, 2));
     }
   })
   client.on('close', (had_err) => console.log(had_err))
@@ -49,6 +85,6 @@ server.listen(port, () => {
 // buf2 = Buffer.from('Ə');
 // // buf3 = Buffer.allocUnsafe(2);
 // // buf3.writeUInt16BE(123456789)
-
+// buf2.map(byte => console.log(byte))
 // // console.log(Buffer.concat([buf1,buf2]))
 // console.log(buf2.length)
